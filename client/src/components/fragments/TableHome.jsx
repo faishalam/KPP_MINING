5f4b157d5539d07c415c09e5d8ba23ed052e8782
faishalam <faishalabdulmajid@gmail.com>
2024-05-07 14:11:37 +0700

    percentage

--- client/src/components/fragments/TableHome.jsx
+++ client/src/components/fragments/TableHome.jsx
@@ -68,12 +68,195 @@ export default function TableHome(props) {
 
     return (
         <>
-            <div className="flex flex-col bg-white mt-10 rounded-md p-2">
+            <div className="flex flex-col bg-white mt-10 rounded-md shadow-md p-2 overflow-auto">
+                <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div className="inline-block min-w-full sm:px-6 lg:px-8">
+                        <div className="overflow-hidden">
+                            <table
+                                className="min-w-full border border-neutral-200 text-start text-sm font-light text-surface border:black text-black">
+                                <thead
+                                    className="border-b border-neutral-200 font-medium">
+                                    <tr>
+                                        <th
+                                            scope="col"
+                                            className="border-e border-neutral-200 px-6 py-4">
+                                            No
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="border-e border-neutral-200 px-6 py-4">
+                                            Nama
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="border-e border-neutral-200 px-6 py-4">
+                                            Nilai
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="border-e border-neutral-200 px-6 py-4">
+                                            Quantity
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="border-e border-neutral-200 px-6 py-4">
+                                            Total Nilai
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="border-e border-neutral-200 px-6 py-4">
+                                            Depresiasi
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="border-e border-neutral-200 px-6 py-4">
+                                            Benefit
+                                        </th>
+
+                                        <th
+                                            scope="col"
+                                            className="border-e border-neutral-200 px-6 py-4">
+                                            By
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="border-e border-neutral-200 px-6 py-4">
+                                            Status
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="border-e border-neutral-200 px-6 py-4">
+                                            Realisasi
+                                        </th>
+
+                                        {
+                                            type === "byUser" && (
+                                                <th
+                                                    scope="col"
+                                                    className="border-e border-neutral-200 px-6 py-4">
+                                                    Actions
+                                                </th>
+                                            )
+                                        }
+
+                                        {
+                                            type === "department" && (
+                                                <th
+                                                    scope="col"
+                                                    className="border-e border-neutral-200 px-6 py-4">
+                                                    Actions
+                                                </th>
+                                            )
+                                        }
+
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {data?.map((item, i) => (
+                                        <tr className="border-b border-neutral-200">
+                                            <td
+                                                className="whitespace-nowrap border-e border-neutral-200 px-6 py-4 font-medium">
+                                                {i + 1}
+                                            </td>
+                                            <td
+                                                className="whitespace-nowrap border-e border-neutral-200 px-6 py-4">
+                                                {item.namaAsset}
+                                            </td>
+                                            <td
+                                                className="whitespace-nowrap border-e border-neutral-200 px-6 py-4">
+                                                Rp. {item.nilaiAsset.toLocaleString('id-ID')}
+                                            </td>
+                                            <td
+                                                className="whitespace-nowrap border-e border-neutral-200 px-6 py-4">
+                                                {item.quantityAsset}
+                                            </td>
+                                            <td
+                                                className="whitespace-nowrap border-e border-neutral-200 px-6 py-4">
+                                                Rp. {item.totalNilaiAsset.toLocaleString('id-ID')}
+                                            </td>
+                                            <td
+                                                className="whitespace-nowrap border-e border-neutral-200 px-6 py-4">
+                                                {item.depresiasi}
+                                            </td>
+                                            <td
+                                                className="whitespace-wrap border-e border-neutral-200 px-6 py-4">
+                                                {item.benefit}
+                                            </td>
+                                            <td
+                                                className="whitespace-nowrap border-e border-neutral-200 px-6 py-4 italic text-[#aaa7a7]">
+                                                {item.User.username}
+                                            </td>
+                                            <td className={`whitespace-nowrap border-e border-neutral-200 px-6 py-4 italic ${item.status === "approved" ? "text-green-600" : "text-red-600"}`}>
+                                                {item.status}
+                                            </td>
+                                            <td
+                                                className="whitespace-nowrap border-e border-neutral-200 px-6 py-4">
+                                                {new Date(item.planRealisasi).toISOString().split('T')[0]}
+                                            </td>
+
+                                            {
+                                                type === "byUser" && (
+                                                    <td className="whitespace-nowrap px-6 py-4">
+                                                        <ButtonElement
+                                                            classname="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-2 rounded focus:outline-none focus:shadow-outline mr-2"
+                                                            type="button"
+                                                            handleClick={() => onHandleDelete(item.id)}
+                                                        >
+                                                            Delete
+                                                        </ButtonElement>
+
+                                                        <ButtonElement
+                                                            classname="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                                            type="button"
+                                                            handleClick={() => onHandleEdit(item.id)}
+                                                        >
+                                                            Edit
+                                                        </ButtonElement>
+                                                        <ModalEditAsset id={item?.id} />
+                                                    </td>
+                                                )
+                                            }
+
+                                            {
+                                                type === "department" && (
+                                                    <td className="whitespace-nowrap px-6 py-4">
+                                                        {item.status === "waiting" ? (
+                                                            <ButtonElement
+                                                                classname={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded focus:outline-none focus:shadow-outline mr-2`}
+                                                                type="button"
+                                                                handleClick={() => onHandleApprove(item.id)}
+                                                            >
+                                                                Approve
+                                                            </ButtonElement>
+                                                        ) : (
+                                                            <ButtonElement
+                                                                classname={`text-green-500 italic font-bold py-2 px-2 rounded focus:outline-none focus:shadow-outline mr-2`}
+                                                                type="button"
+                                                            >
+                                                                Approved
+                                                            </ButtonElement>
+                                                        )}
+
+                                                    </td>
+                                                )
+                                            }
+
+
+                                        </tr>
+
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* <div className="flex flex-col bg-white mt-10 rounded-md p-2">
                 <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
                         <div className="overflow-hidden">
-                            <table className="min-w-full text-left text-sm font-light text-surface dark:text-white">
-                                <thead className="border-b border-neutral-200 font-medium dark:border-white/10">
+                            <table className="min-w-full text-left text-sm font-light text-surface text-black">
+                                <thead className="border-b border-neutral-200 font-medium">
                                     <tr>
                                         <th scope="col" className="px-6 py-4">
                                             No
@@ -131,7 +314,7 @@ export default function TableHome(props) {
                                             <td className="whitespace-nowrap px-6 py-4">{item.namaAsset}</td>
                                             <td className="whitespace-nowrap px-6 py-4">Rp. {item.nilaiAsset.toLocaleString('id-ID')}</td>
                                             <td className="whitespace-nowrap px-6 py-4">{item.quantityAsset}</td>
-                                            <td className="whitespace-nowrap px-6 py-4">{item.totalNilaiAsset.toLocaleString('id-ID')}</td>
+                                            <td className="whitespace-nowrap px-6 py-4">Rp. {item.totalNilaiAsset.toLocaleString('id-ID')}</td>
                                             <td className="whitespace-nowrap px-6 py-4">{item.depresiasi}</td>
                                             <td className="whitespace-wrap px-6 py-4">{item.benefit}</td>
                                             <td className="whitespace-nowrap px-6 py-4 text-gray-400 italic">{item.User.username}</td>
@@ -191,7 +374,7 @@ export default function TableHome(props) {
                         </div>
                     </div>
                 </div>
-            </div >
+            </div > */}
         </>
     )
 }
--- client/src/pages/HomePages.jsx
+++ client/src/pages/HomePages.jsx
@@ -1,3 +1,8 @@
+import { RiFileReduceLine } from "react-icons/ri";
+import { BsCashCoin } from "react-icons/bs";
+import { CiNoWaitingSign } from "react-icons/ci";
+import { FcAcceptDatabase } from "react-icons/fc";
+import { TEChart } from "tw-elements-react";
 import { useDispatch, useSelector } from "react-redux";
 import Sidebar from "../components/fragments/Sidebar";
 import { getAllAsset, getAssetByUser } from "../features/asset/asyncAction";
@@ -33,6 +38,13 @@ export default function HomePages(props) {
         getData()
     }, [dispatch, type])
 
+    const totalAssets = asset?.length || 0; // Total jumlah asset
+
+    const waitingCount = asset?.filter((a) => a.status === "waiting").length; // Jumlah asset yang menunggu
+    const approveCount = asset?.filter((a) => a.status === "approved").length; // Jumlah asset yang disetujui
+
+    const waitingPercentage = (waitingCount / totalAssets) * 100; // Persentase asset yang menunggu
+    const approvePercentage = (approveCount / totalAssets) * 100; // Persentase asset yang disetujui
 
 
     return (
@@ -47,22 +59,77 @@ export default function HomePages(props) {
                         <h1 className="text-xl font-bold mb-6">Dashboard |</h1>
                     </div>
                     <div className="flex gap-6">
-                        <div className="w-1/4 h-20 bg-white rounded-md flex justify-center items-center">
+                        <div className="w-1/5 h-20 bg-white rounded-md flex justify-center items-center shadow-md">
+                            <RiFileReduceLine className="text-4xl mr-2" />
                             <p className="text-sm">Total Asset : {asset?.length}</p>
                         </div>
-                        <div className="w-1/4 h-20 bg-white rounded-md flex justify-center items-center">
-                            <p className="text-sm">Total Nilai :Rp. {asset?.reduce((a, b) => a + b.totalNilaiAsset, 0).toLocaleString('id-ID')}</p>
+                        <div className="w-1/5 h-20 bg-white rounded-md flex justify-center items-center shadow-md">
+                            <BsCashCoin className="text-4xl mr-4" fill="green" />
+                            <div className="flex flex-col">
+                                <p className="text-sm">Total Nilai</p>
+                                <p className="text-sm">Rp. {asset?.reduce((a, b) => a + b.totalNilaiAsset, 0).toLocaleString('id-ID')}</p>
+                            </div>
                         </div>
-                        <div className="w-1/4 h-20 bg-white rounded-md flex justify-center items-center">
-                            <p className="text-sm">waiting : {asset?.filter((a) => a.status === "waiting").length}</p>
+                        <div className="w-1/5 h-20 bg-white rounded-md flex justify-center items-center shadow-md">
+                            <CiNoWaitingSign className="text-4xl mr-2" fill="red" />
+                            <p className="text-sm">Waiting : {asset?.filter((a) => a.status === "waiting").length}</p>
                         </div>
-                        <div className="w-1/4 h-20 bg-white rounded-md flex justify-center items-center">
-                            <p className="text-sm">approve : {asset?.filter((a) => a.status === "approved").length}</p>
+                        <div className="w-1/5 h-20 bg-white rounded-md flex justify-center items-center shadow-md">
+                            <FcAcceptDatabase
+                                className="text-4xl mr-2" />
+                            <p className="text-sm">Approve : {asset?.filter((a) => a.status === "approved").length}</p>
                         </div>
+
+                        <div className="w-1/5 h-20 bg-white rounded-md flex justify-center items-center shadow-md p-2">
+                            <div className="w-full h-full ">
+                                <TEChart
+                                    type="pie"
+                                    data={{
+                                        labels: ["Waiting", "Approved"],
+                                        datasets: [
+                                            {
+                                                label: "Status",
+                                                data: [waitingPercentage, approvePercentage],
+                                                backgroundColor: [
+                                                    "rgba(255, 99, 132, 0.5)",
+                                                    "rgba(77, 182, 172, 0.5)",
+                                                ],
+                                            },
+                                        ],
+                                    }}
+                                    options={{
+                                        plugins: {
+                                            legend: {
+                                                position: "right", // Atur posisi label di samping chart
+                                            },
+                                            tooltip: {
+                                                callbacks: {
+                                                    label: (context) => {
+                                                        let label = context.label || "";
+                                                        if (label) {
+                                                            label += ": ";
+                                                        }
+                                                        if (context.parsed) {
+                                                            label += context.parsed.toFixed(2) + "%";
+                                                        }
+                                                        return label;
+                                                    },
+                                                },
+                                            },
+                                        },
+                                        maintainAspectRatio: false, // Memastikan chart tidak mempertahankan rasio aspek
+                                        maxWidth: "100%",
+                                        maxHeight: "100%",
+                                    }}
+                                    className="w-full h-10"
+                                />
+                            </div>
+                        </div>
+
                     </div>
                     <TableHome data={asset} type={type} />
                 </div>
-            </div>
+            </div >
         </>
     )
 }
