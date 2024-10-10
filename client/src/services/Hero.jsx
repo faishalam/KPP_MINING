d6638390fe262bf847629ebbc8170e628374f2f2
faishalam <faishalabdulmajid@gmail.com>
2024-10-10 19:58:45 +0700

    done

--- client/src/services/Hero.jsx
+++ client/src/services/Hero.jsx
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
-// const baseUrl = 'http://34.142.247.224/';
-const baseUrl = 'http://localhost:3000/';
+const baseUrl = 'https://server.kppmonitoring.online/';
+// const baseUrl = 'http://localhost:3000/';
 
 export const heroService = Axios.create({
     baseURL: baseUrl,
