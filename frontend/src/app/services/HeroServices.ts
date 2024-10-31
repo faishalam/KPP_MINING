12ebf3eb966b0e73ebce7cd11e04f2aee70945e7
faishalam <faishalabdulmajid@gmail.com>
2024-10-31 13:00:44 +0700

    fix deploy fe

--- frontend/src/app/services/HeroServices.ts
+++ frontend/src/app/services/HeroServices.ts
@@ -1,7 +1,8 @@
 
 import Axios from "axios";
 
-const baseUrl = 'http://localhost:3000/'
+// const baseUrl = 'http://localhost:3000/'
+const baseUrl = 'https://server.kppmonitoring.online/'
 
 export const heroService = Axios.create({
     baseURL: baseUrl
