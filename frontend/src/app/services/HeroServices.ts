b2bf11834b2a031666fbce6b45d94c9d81655464
faishalam <faishalabdulmajid@gmail.com>
2024-11-10 19:22:55 +0700

    remove client v1

--- frontend/src/app/services/HeroServices.ts
+++
@@ -1,9 +0,0 @@
-
-import Axios from "axios";
-
-// const baseUrl = 'http://localhost:3000/'
-const baseUrl = 'https://server.kppmonitoring.online/'
-
-export const heroService = Axios.create({
-    baseURL: baseUrl
-});
